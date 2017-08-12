a7164fd433cf6d448fc8bb9930dd340b84e73f6c
Emmanuel Ndukwe <etechgist@gmail.com>
2017-08-12 18:00:39 +0100

    corrected the CORS error for good

--- RandomQuotes/js/app.js
+++ RandomQuotes/js/app.js
@@ -6,28 +6,31 @@ function openURL(url) {
     window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
 }
 $(document).ready(function() {
-    var url = "http://quotes.stormconsultancy.co.uk/random.json";
+    var url = "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en";
 
     function getQuotes() {
         $.ajax({
             url: url,
             type: "Get",
-            dataType: 'json',
+            dataType: 'jsonp',
+            data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+
             success: function(data) {
                 console.log(JSON.stringify(data));
                 $('.tweet-logo').on('click', function() {
                     openURL('https://twitter.com/intent/tweet?hashtags=freecodecamp&related=freecodecamp&text=' +
-                        data.quote + " - " + data.author);
+                        data.quoteText + " - " + data.quoteAuthor);
                 })
-                if (data.quote.length < 180) {
-                    $('.displayQuotes').text(data.quote);
-                    $('#author').text(data.author);
+                if (data.quoteText.length < 180) {
+                    $('.displayQuotes').text(data.quoteText);
+                    $('#author').text(data.quoteAuthor);
                 } else {
                     getQuotes();
                 }
             },
-            error: function(response) {
-                console.log(JSON.stringify(response));
+            error: function(data) {
+                console.log(JSON.stringify(data));
+                $('.displayQuotes').text("something went wrong");
             }
         });
 
