9ab759ac4e0aaccd8ac0ad3f56f7bb3b1a479744
Emmanuel Ndukwe <emasysnd@gmail.com>
2017-09-02 09:56:20 +0100

    refactored fccCalc

--- fccCalc/js/script.js
+++ fccCalc/js/script.js
@@ -4,66 +4,70 @@
 
 let answer = document.getElementById('results');
 let expressions = document.getElementById('expressions');
-$(document).ready(function(){
-        $('#err').hide();
+$(document).ready(function () {
+    $('#err').hide();
 
-    $('#buttons').on('click', function(e) {
+    $('#buttons').on('click', function (e) {
 
         if (e.target.nodeName === 'LI') {
             let value = e.target.innerHTML;
+            if (value === 'x') {
+                value = '*';
+            }
+
+            if (value === '÷') {
+                value = '/';
+            }
             if (value === '=') {
                 try {
-                    if(expressions.innerHTML !== ""){
+                    if (expressions.innerHTML !== "") {
                         answer.innerHTML = eval(expressions.innerHTML);
                     }
 
-                } catch(e) {
-                    $('#results').css({"fontSize":"26px","fontWeight":"Bolder"});
+                } catch (e) {
+                    $('#results').css({"fontSize": "26px", "fontWeight": "Bolder"});
                     answer.innerHTML = "sign error";
                 }
-            } else if(value ==='AC'){
+            } else if (value === 'AC') {
                 expressions.innerHTML = "";
                 answer.innerHTML = "";
-                $('#results').css({"fontSize":"26px","fontWeight":"Bolder"});
-                $('#expressions').css("fontSize","28px");
-                $('.typed-cursor').css("fontSize","28px");
-            }
-            else if(value ==='C'){
+                $('#results').css({"fontSize": "26px", "fontWeight": "Bolder"});
+                $('#expressions').css("fontSize", "28px");
+                $('.typed-cursor').css("fontSize", "28px");
+            } else if (value === 'DEL') {
                 var c = expressions.innerHTML;
-                if(expressions.innerHTML.length < 14) {
-                    expressions.innerHTML = c.slice(0,c.length-1);
+                if (expressions.innerHTML.length < 14) {
+                    expressions.innerHTML = c.slice(0, c.length - 1);
                     answer.innerHTML = "";
-                    $('#results').css({"fontSize":"26px","fontWeight":"Bolder"});
-                    $('#expressions').css("fontSize","28px");
-                    $('.typed-cursor').css("fontSize","28px");
+                    $('#results').css({"fontSize": "26px", "fontWeight": "Bolder"});
+                    $('#expressions').css("fontSize", "28px");
+                    $('.typed-cursor').css("fontSize", "28px");
 
-                }else{
-                    expressions.innerHTML = c.slice(0,c.length-1);
+                } else {
+                    expressions.innerHTML = c.slice(0, c.length - 1);
                     answer.innerHTML = "";
-                    if(expressions.innerHTML.length === 28) {
+                    if (expressions.innerHTML.length === 28) {
                         $('#err').show();
                     }
                 }
-            }
-            else{
-                if(expressions.innerHTML.length <= 11) {
+            } else {
+                if (expressions.innerHTML.length <= 11) {
                     $('#err').hide();
                     expressions.innerHTML += value;
-                    $('#results').css("fontSize","40px");
-                    $('#expressions').css("fontSize","28px");
-                    $('.typed-cursor').css("fontSize","28px");
-                }else if(expressions.innerHTML.length === 28) {
+                    $('#results').css("fontSize", "40px");
+                    $('#expressions').css("fontSize", "28px");
+                    $('.typed-cursor').css("fontSize", "28px");
+                } else if (expressions.innerHTML.length === 28) {
                     $('#err').show();
-                }else{
+                } else {
                     $('#err').hide();
                     expressions.innerHTML += value;
-                    $('#results').css("fontSize","16px");
-                    $('#expressions').css("fontSize","12px");
-                    $('.typed-cursor').css("fontSize","12px");
+                    $('#results').css("fontSize", "16px");
+                    $('#expressions').css("fontSize", "12px");
+                    $('.typed-cursor').css("fontSize", "12px");
                 }
             }
         }
     });
 
 });
-
