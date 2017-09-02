0d268feb0087de1188b7009a3b6237d100e8c5ed
Emmanuel Ndukwe <emasysnd@gmail.com>
2017-09-02 13:17:33 +0100

    added thumbs

--- weatherApp/api.js
+++ weatherApp/api.js
@@ -1,26 +1,29 @@
 /**
  * Created by EMASYS ND on 7/6/2017.
  */
-var f, s, t;
+var f,
+    s,
+    t;
 
 function weather() {
 
-    var location = document.getElementById("location");
-    var apiKey = '17eca980f3421907cabe87450e4de798';
-    var url = 'https://api.forecast.io/forecast/';
-    var url1 = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='
+    var location = document.getElementById("location"),
+        apiKey = '17eca980f3421907cabe87450e4de798',
+        url = 'https://api.forecast.io/forecast/',
+        geocode = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=',
+        geocodekey = '&key=AIzaSyDJrBZqHselB1Xim5ft5r1bI65_5gOOpvA';
 
-    navigator.geolocation.getCurrentPosition(success, error);
+    navigator
+        .geolocation
+        .getCurrentPosition(success, error);
 
     function success(position) {
         latitude = position.coords.latitude;
         longitude = position.coords.longitude;
 
-        location.innerHTML = latitude + ', '+ longitude;
+        location.innerHTML = latitude + ', ' + longitude;
 
-        $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
-            t  = "this service is no longer supported, Although we know you're somewhere in " +data.timezone;
-
+        $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function (data) {
 
             f = Math.floor(data.currently.temperature);
             s = data.currently.summary;
@@ -30,61 +33,68 @@ function weather() {
 
             var condition = data.currently.icon;
 
-            if(condition == "partly-cloudy-day"){
+            if (condition == "partly-cloudy-day") {
 
-                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/partly_cloudy.jpg\")");
-
+                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/p" +
+                        "artly_cloudy.jpg\")");
 
             }
-            if(condition == "wind") {
+            if (condition == "wind") {
 
-                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/wind.jpg\")");
+                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/w" +
+                        "ind.jpg\")");
             }
-            if(condition == "partly-cloudy-night") {
+            if (condition == "partly-cloudy-night") {
 
-                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/partly-night.jpg\")");
-            }
-
-            if(condition == "fog") {
-
-                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/fog.jpg\")");
+                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/p" +
+                        "artly-night.jpg\")");
             }
 
-            if(condition == "cloudy") {
+            if (condition == "fog") {
 
-                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/cloudy.jpg\")");
+                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/f" +
+                        "og.jpg\")");
             }
 
-            if(condition == "rain") {
+            if (condition == "cloudy") {
 
-                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/rain.jpg\")");
-            }
-            if(condition == "snow") {
-
-                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/sleet.jpeg\")");
-            }
-            if(condition == "sleet") {
-
-                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/snow.jpg\")");
-            }
-            if(condition == "clear-day") {
-
-                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/clearDay.jpg\")");
-            }
-            if(condition == "clear-night") {
-
-                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/clear-night.png\")");
+                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/c" +
+                        "loudy.jpg\")");
             }
 
+            if (condition == "rain") {
+
+                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/r" +
+                        "ain.jpg\")");
+            }
+            if (condition == "snow") {
+
+                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/s" +
+                        "leet.jpeg\")");
+            }
+            if (condition == "sleet") {
+
+                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/s" +
+                        "now.jpg\")");
+            }
+            if (condition == "clear-day") {
+
+                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/c" +
+                        "learDay.jpg\")");
+            }
+            if (condition == "clear-night") {
+
+                $('.site-wrapper').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/c" +
+                        "lear-night.png\")");
+            }
 
         });
 
-        $.getJSON(url1 + latitude + "," + longitude + "&sensor=true", function(data) {
-            if(data.length < 1){
-                $('#city').html(data.results[0].formatted_address);
-            }else{
-                $('#info').html("Real time weather forecast<br>for your current location");
-            }
+        $.getJSON(geocode + latitude + "," + longitude + geocodekey, function (data) {
+            console.log(data);
+
+            $('#city').html(data.results[0].formatted_address);
+
         });
 
     }
@@ -96,24 +106,25 @@ function weather() {
     location.innerHTML = "Locating...";
 }
 
-
 weather();
 
-$(document).ready(function(){
-
+$(document).ready(function () {
 
     $("#convFtoC").hide();
-    $("#convCtoF").click(function(){
-        var c = (5/9) * (f - 32);
-        document.getElementById("temp").innerHTML = Math.floor(c) + "°C";
+    $("#convCtoF").click(function () {
+        var c = (5 / 9) * (f - 32);
+        document
+            .getElementById("temp")
+            .innerHTML = Math.floor(c) + "°C";
         $("#convCtoF").hide();
         $("#convFtoC").show();
     });
 
-    $("#convFtoC").click(function() {
-        document.getElementById("temp").innerHTML = f + "°F";
+    $("#convFtoC").click(function () {
+        document
+            .getElementById("temp")
+            .innerHTML = f + "°F";
         $("#convFtoC").hide();
         $("#convCtoF").show();
     });
 });
-
