1fd7a85e0ee5c6fc9bd7f3c81bcc05af24867bdf
Emmanuel Ndukwe <emasysnd@gmail.com>
2017-09-10 16:08:34 +0100

    redesigned the weather app

--- weatherApp/js/api.js
+++ weatherApp/js/api.js
@@ -1,9 +1,57 @@
 /**
  * Created by EMASYS ND on 7/6/2017.
  */
-var f,
+let f,
     s,
     t;
+
+const days = date => {
+    var timestamp = moment.unix(date);
+    return timestamp.format("dddd");
+}
+
+const getIcon = (icon, classname, idName) => {
+    if (icon === 'clear-day') {
+        $(classname).css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/c" +
+                "learDay.jpg\")");
+        $(idName).addClass('ion-ios-sunny');
+    } else if (icon === 'clear-night') {
+        $(classname).css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/c" +
+                "lear-night.png\")");
+        $(idName).addClass('ion-ios-moon');
+    } else if (icon === 'partly-cloudy-day') {
+        $(classname).css("backgroundImage", "url(\"images/partly-cloudy.jpg\")");
+        $(idName).addClass('ion-ios-partlysunny');
+    } else if (icon === 'partly-cloudy-night') {
+        $(classname).css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/p" +
+                "artly-night.jpg\")");
+        $(idName).addClass('ion-ios-cloudy-night');
+    } else if (icon === 'cloudy') {
+        $(classname).css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/c" +
+                "loudy.jpg\")");
+        $(idName).addClass('ion-ios-cloudy');
+    } else if (icon === 'rain') {
+        $(classname).css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/r" +
+                "ain.jpg\")");
+        $(idName).addClass('ion-ios-rainy');
+    } else if (icon === 'sleet') {
+        $(classname).css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/s" +
+                "now.jpg\")");
+        $(idName).addClass('ion-ios-rainy');
+    } else if (icon === 'snow') {
+        $(classname).css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/s" +
+                "leet.jpeg\")");
+        $(idName).addClass('ion-ios-snowy');
+    } else if (icon === 'wind') {
+        $(classname).css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/w" +
+                "ind.jpg\")");
+        $(idName).addClass('ion-leaf');
+    } else if (icon === 'fog') {
+        $(classname).css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/f" +
+                "og.jpg\")");
+        $(idName).addClass('ion-ios-analytics');
+    }
+}
 
 function weather() {
 
@@ -24,74 +72,41 @@ function weather() {
 
             f = Math.floor(data.currently.temperature);
             s = data.daily.summary;
-            $('#temp').html(f + '°F');
+            $('#temp').html(f + "<em>°F</em>");
             $('#minutely').html(s);
             $('#today').html(data.currently.summary);
             $('#bg').html(data.currently.icon);
+            $('#summary').html(data.hourly.summary);
 
-            var condition = data.currently.icon;
+            // var timestamp = moment.unix(data.daily.data[0].time);
 
-            if (condition == "partly-cloudy-day") {
+            var i = 0;
 
-                $(' .img-sync').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/p" +
-                        "artly_cloudy.jpg\")");
+            if (data) {
+                data
+                    .daily
+                    .data
+                    .map((info) => {
+                        i++;
+                        // days(date.time);
+                        $('#date' + i).html(days(info.time));
+                        getIcon(info.icon, null, '#main' + i);
+                        $('#temperature' + i).html(Math.floor(info.temperatureMax) + "<em>°F</em>");
+                        $('#status' + i).html(info.icon)
+                    })
 
             }
-            if (condition == "wind") {
 
-                $(' .img-sync').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/w" +
-                        "ind.jpg\")");
-            }
-            if (condition == "partly-cloudy-night") {
+            let condition = data.currently.icon,
+                classname = '.bgImg',
+                idName = '#main';
 
-                $(' .img-sync').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/p" +
-                        "artly-night.jpg\")");
-            }
-
-            if (condition == "fog") {
-
-                $(' .img-sync').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/f" +
-                        "og.jpg\")");
-            }
-
-            if (condition == "cloudy") {
-
-                $(' .img-sync').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/c" +
-                        "loudy.jpg\")");
-            }
-
-            if (condition == "rain") {
-
-                $(' .img-sync').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/r" +
-                        "ain.jpg\")");
-            }
-            if (condition == "snow") {
-
-                $(' .img-sync').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/s" +
-                        "leet.jpeg\")");
-            }
-            if (condition == "sleet") {
-
-                $(' .img-sync').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/s" +
-                        "now.jpg\")");
-            }
-            if (condition == "clear-day") {
-
-                $(' .img-sync').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/c" +
-                        "learDay.jpg\")");
-            }
-            if (condition == "clear-night") {
-
-                $(' .img-sync').css("backgroundImage", "url(\"https://raw.githubusercontent.com/emasys/webDev/master/weatherApp/images/c" +
-                        "lear-night.png\")");
-            }
+            getIcon(condition, classname, idName);
 
         });
 
         $.getJSON(geocode + latitude + "," + longitude + geocodekey, function (data) {
-            console.log(data);
-
-            $('#city').html(data.results[0].formatted_address);
+            $('#city').html(data.results[1].formatted_address);
 
         });
 
@@ -105,32 +120,40 @@ function weather() {
 
 weather();
 
-$(document).ready(function () {
+const getCel = feh => {
+    let cel = eval((feh - 32) * .5556);
+    return Math.floor(cel);
+}
 
-    $('#temp')
-        .on('click', function () {
-            if ($(this).val === (f + '°F')) {
-                var c = (5 / 9) * (f - 32);
-                $(this).text(Math.floor(c) + "°C");
-                console.log("something");
+const getFeh = cel => {
+    let feh = eval(cel * 1.8 + 32);
+    return Math.floor(feh);
+}
+
+$(document)
+    .ready(function () {
+
+        $('#cel').on('click', () => {
+            if (!$('#cel').hasClass('clicked')) {
+                let v = getCel(f);
+                $('#temp').html(v + "<em>°C</em>");
+                $('#cel').addClass('clicked');
+                $('#fer').removeClass('clicked');
             }
+
         })
 
-    $("#convFtoC").hide();
-    $("#convCtoF").click(function () {
-        var c = (5 / 9) * (f - 32);
-        document
-            .getElementById("temp")
-            .innerHTML = Math.floor(c) + "°C";
-        $("#convCtoF").hide();
-        $("#convFtoC").show();
-    });
+        $('#fer').on('click', () => {
+            console.log();
+            if ($('#cel').hasClass('clicked') && !$('#fer').hasClass('clicked')) {
+                let v = $('#temp').text();
+                v = v.substring(0, 2);
+                v = getFeh(v);
+                $('#temp').html(v + "<em>°F</em>");
+                $('#fer').addClass('clicked');
+                $('#cel').removeClass('clicked');
+            }
 
-    $("#convFtoC").click(function () {
-        document
-            .getElementById("temp")
-            .innerHTML = f + "°F";
-        $("#convFtoC").hide();
-        $("#convCtoF").show();
+        })
+
     });
-});
