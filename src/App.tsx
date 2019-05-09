f36a948700bedb9918253297998eeed564181a27
Emmanuel Ndukwe <emasysnd@gmail.com>
2019-05-09 15:33:14 +0100

    bootstrap ts app with cra

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import './styles/App.scss';
+
+const App: React.FunctionComponent<{}> = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="container">
+          <div className="card">
+            card
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- wiki/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', main);
-
-function main() {
-    titlebarJs.init({isTop: true, id: 'header', triggerId: 'xx'});
-    // titlebarJs.init({id: 'titlebarbottom', isTop: false});
-}
