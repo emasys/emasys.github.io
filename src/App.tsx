53980f0980f443271eebc091d80dd48c4b15e4e1
Emmanuel Ndukwe <emasysnd@gmail.com>
2019-11-19 21:22:50 +0100

    start working on portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import * as React from 'react';
-import Card from './Card';
 import './styles/App.scss';
 
 const App: React.FunctionComponent<{}> = () => {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="container">
-          <div className="row justify-content-center mt-5">
-            <div className="col-12 col-md-4 card-wrapper">
-              <Card>contents</Card>
-              <Card>second contents</Card>
-              <p>Thank you!</p>
-            </div>
-          </div>
-        </div>
+    <div className='container'>
+      <div className='card'>
+        <p>Hi there, this page is currently under construction. </p>
+        <p>
+          However, you can still connect with me via{' '}
+          <a href='https://www.linkedin.com/in/emmanuel-ndukwe/'>LinkedIn</a>
+        </p>
       </div>
     </div>
   );
-}
+};
 
 export default App;
