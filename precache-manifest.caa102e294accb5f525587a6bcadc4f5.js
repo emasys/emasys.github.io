c5d1d644e2a16972a5e0d3899903aad1ff5aef2b
Emmanuel Ndukwe <emasysnd@gmail.com>
2019-11-20 09:53:27 +0100

    Updates

---
+++ precache-manifest.caa102e294accb5f525587a6bcadc4f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83dfd9381f0fb505b16f6fa45a38bf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21a4e22c600f19d1006b",
+    "url": "/static/css/main.8e9104e5.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "21a4e22c600f19d1006b",
+    "url": "/static/js/main.83dd4dc9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
